c5e52b6a361d8a6ffddb2da1adad87370e5aa9c0
Narnianknight <83618342+Narnianknight@users.noreply.github.com>
2025-05-05 15:54:44 -0400

    Change to Java repos

--- unit15_jschrock4-2.py
+++ unit15_jschrock4-2.py
@@ -1,4 +1,5 @@
 import requests
+import plotly.express as px
 
 url = "https://api.github.com/search/repositories"
 url += "?q=language:java+sort:stars+stars:>10000"
@@ -23,4 +24,7 @@ for repo_dict in repo_dicts:
     hover_text = f"{owner}<br>{description}"
     hover_texts.append(hover_text)
 
-print(repo_links, stars, hover_texts)
+title = "Most Starred Java Repos on Github"
+labels = {"x": "Repository", "y": "Stars"}
+fig = px.bar(x=repo_links, y=stars, hover_name=hover_texts, title=title, labels=labels)
+fig.show()
